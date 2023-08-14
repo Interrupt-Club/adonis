b5f5494f2edaadd6979a3cbf8c5aa74a095b2ea9
dv-interrupt-club <main.interrupt.club@outlook.com>
2023-08-14 16:32:04 +0530

    Base Commit
    base commit

---
+++ constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+    WEBSITE_TITLE: "Interrupt Club",
+    WEBSITE_TITLE_SHORT: "Interrupt",
+    WEBSITE_TAGLINE: "Empowering Your Tech Journey: From Learning to Landing",
+
+    // SEO
+    WEBSITE_DESCRIPTION: "Learn technical skills, ace interviews and land software engineer job. Your journey starts at Interrupt Club",
+
+    // WEBSITE ROUTING
+    WEBSITE_BASE_URL: "/",
+    WEBSITE_COMPLETE_URL: "https://www.interrupt.club"
+}
---
+++ gatsby-browser.js
@@ -0,0 +1,13 @@
+// custom typefaces
+// import '@fontsource/syne/400.css';
+// import '@fontsource/syne/700.css';
+
+// normalize CSS across browsers
+import "./src/styles/normalize.css"
+
+// custom CSS styles
+import "./src/styles/theme.css"
+import "./src/styles/main.css"
+
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
