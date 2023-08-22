c7f97cdb2a3f19364c2915da1171e1f0c20c2f59
dv-interrupt-club <main.interrupt.club@outlook.com>
2023-08-22 08:35:13 +0530

    Password
    Password

--- scripts/unzip.js
+++ scripts/unzip.js
@@ -1,6 +1,6 @@
 const {spawn} = require("child_process")
 
-var newFile = spawn('unzip', [ '-P','123', '-d','./dist','./dist/target.zip' ])
+var newFile = spawn('unzip', [ '-P',process.env.EXTRACT_KEY, '-d','./dist','./dist/target.zip' ])
 
 newFile.stdout.on("data", (data) => {
     console.log(data.toString())
